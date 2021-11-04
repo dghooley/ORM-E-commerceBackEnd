7577e6d675acff7a1fd286b7a852871a55296f67
Derick Hooley <dghooley@gmail.com>
2021-11-04 10:36:11 -0500

    addedDataBaseModels/associations

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -1,6 +1,6 @@
 -- DROP DATABASE
-DROP DATABASE IF EXISTS ecommerce_db;
+DROP DATABASE IF EXISTS ORM_E_commerce_backEnd_db;
 
 -- CREATE DATABASE
-CREATE DATABASE ecommerce_db;
+CREATE DATABASE ORM_E_commerce_backEnd_db;
 
